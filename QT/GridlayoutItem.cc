8038566cffeed6c167f53a24264a7f51c2a68a24
Ijang6 <18628149736@163.com>
2019-10-28 22:31:04 +0800

    QGridLayout Item

---
+++ QT/GridlayoutItem.cc
@@ -0,0 +1,15 @@
+
+#include <QGridLayout>
+QGridLayout *gridlayout = new QGridLayout;
+
+for(int index =0;index < gridlayout->count();++index)
+{
+  QPushButton *pbutton = qobject_cast<QPushButton*>(gridlayout->itemAt(index)->widget()));
+
+  if(pbutton != nullptr)
+  {
+  
+    xxxxx;
+  }
+
+}
